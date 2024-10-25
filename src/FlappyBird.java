d28a6af337ac743e6ab52cae9d67cd1d85c968a9
Sam Devine <samuel.will.devine@gmail.com>
2024-10-25 15:19:56 -0600

    draw background image in game window

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,8 +8,28 @@ public class FlappyBird extends JPanel {
     int boardWidth = 360;
     int boardHeight = 640;
 
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
+    // Constructor
     FlappyBird() {
         setPreferredSize(new Dimension(boardWidth, boardHeight));
-        setBackground(Color.blue);
+//        setBackground(Color.blue);
+
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
     }
 }
