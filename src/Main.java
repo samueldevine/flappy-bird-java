ee64b75ac249582a30ac807315642e08f4b765dd
Sam Devine <samuel.will.devine@gmail.com>
2024-10-25 15:10:41 -0600

    add panel to game window

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         int boardWidth = 360;
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
